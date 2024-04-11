8a7452ae40f14385d482d343b0eeffa3588e3eb8
hojunhojun <85822956+hojunhojun@users.noreply.github.com>
2024-04-11 17:37:27 +0900

    Add files via upload

---
+++ 24Y03M/240330/ch22/22_08.js
@@ -0,0 +1,11 @@
+function foo() {
+    'use strict';
+  
+    console.log("foo's this: ", this);  // undefined
+    function bar() {
+      console.log("bar's this: ", this); // undefined
+    }
+    bar();
+  }
+  foo();
+  
