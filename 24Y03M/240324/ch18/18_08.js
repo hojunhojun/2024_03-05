65971fe983d8b4f90a0c96d74a5de7cd35ddf554
hojunhojun <85822956+hojunhojun@users.noreply.github.com>
2024-04-11 17:33:01 +0900

    Add files via upload

---
+++ 24Y03M/240324/ch18/18_08.js
@@ -0,0 +1,8 @@
+// ES6 Rest parameter
+function sum(...args) {
+    return args.reduce((pre, cur) => pre + cur, 0);
+  }
+  
+  console.log(sum(1, 2));          // 3
+  console.log(sum(1, 2, 3, 4, 5)); // 15
+  
---
+++ 24Y03M/240325/ch19/19_01.js
@@ -0,0 +1,8 @@
+// 이름과 주소 속성을 갖는 객체
+const person = {
+    name: 'Lee',
+    address: 'Seoul'
+  };
+  
+  console.log(person); // {name: "Lee", address: "Seoul"}
+  
