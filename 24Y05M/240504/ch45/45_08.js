4dffb7d1325a490dcccddbc88f680685abf6583f
hojunhojun <85822956+hojunhojun@users.noreply.github.com>
2024-05-05 23:45:20 +0900

    Add files via upload

---
+++ 24Y05M/240504/ch45/45_08.js
@@ -0,0 +1,10 @@
+get('/step1', a => {
+    get(`/step2/${a}`, b => {
+      get(`/step3/${b}`, c => {
+        get(`/step4/${c}`, d => {
+          console.log(d);
+        });
+      });
+    });
+  });
+  
