387956823d739bbc231cc75dba58a3f636cb67a5
hojunhojun <85822956+hojunhojun@users.noreply.github.com>
2024-05-09 16:05:34 +0900

    Add files via upload

---
+++ 24Y05M/240507/ch47/47_12.js
@@ -0,0 +1,18 @@
+const foo = () => {
+    throw Error('foo에서 발생한 에러'); // ④
+  };
+  
+  const bar = () => {
+    foo(); // ③
+  };
+  
+  const baz = () => {
+    bar(); // ②
+  };
+  
+  try {
+    baz(); // ①
+  } catch (err) {
+    console.error(err);
+  }
+  
